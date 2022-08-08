1b8b71feae1e14a9f47cf5a1ec99687b33917fe6
trun222 <thomasunderwoodii@gmail.com>
2022-08-08 18:21:44 -0400

    Adjust Fastify to work with fileuploads

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,47 @@
 import Fastify, { FastifyInstance } from 'fastify'
 import { Resize, Reduce, Quality } from './util/commands';
-import { loadFile } from './util/files';
+import { loadFile, writeFile } from './util/files';
+import fileUpload from 'fastify-file-upload';
 
 const server: FastifyInstance = Fastify({})
 
-server.post('/resize', async (request, reply) => {
-  const { dimensions, inputFileName, outputFileName }: any = request.body;
-  Resize({
-    dimensions,
-    inputFileName,
-    outputFileName
-  })
-  // Return a payload 
-  return { file: await loadFile(`./output/${outputFileName}`) };
+server.register(fileUpload, {
+  limits: { fileSize: 3 * 1024 * 1024 },
 })
 
-server.post('/reduce', async (request, reply) => {
-  const { percentage, inputFileName, outputFileName }: any = request.body;
-  // Perform the conversion and upload it to S3 or Wasabi
-  Reduce({
-    inputFileName,
-    outputFileName,
-    percentage,
-  });
-  return { file: await loadFile(`./output/${outputFileName}`) };
-})
+// server.post('/resize', async (request, reply) => {
+//   const { dimensions, inputFileName, outputFileName }: any = request.body;
+//   Resize({
+//     dimensions,
+//     inputFileName,
+//     outputFileName
+//   })
+//   // Return a payload 
+//   return { file: await loadFile(`./output/${outputFileName}`) };
+// })
+
+// server.post('/reduce', async (request, reply) => {
+//   const { percentage, inputFileName, outputFileName }: any = request.body;
+//   // Perform the conversion and upload it to S3 or Wasabi
+//   Reduce({
+//     inputFileName,
+//     outputFileName,
+//     percentage,
+//   });
+//   return { file: await loadFile(`./output/${outputFileName}`) };
+// })
 
 server.post('/quality', async (request, reply) => {
-  const { quality, inputFileName, outputFileName }: any = request.body;
+  const { quality, outputFileName }: any = request.body;
+  const { name, data } = (request.body as any).file;
+  await writeFile(name, './media', data);
   // Perform the conversion and upload it to S3 or Wasabi
   Quality({
-    inputFileName,
+    inputFileName: name,
     outputFileName,
     quality,
   });
-  return { file: await loadFile(`./output/${outputFileName}`) };
+  return { file: await loadFile(outputFileName, 'output') };
 })
 
 const start = async () => {
--- src/util/files.ts
+++ src/util/files.ts
@@ -1,5 +1,20 @@
-import fs from 'fs/promises';
+import fsPromise from 'fs/promises';
+import fs from 'fs';
 
-export const loadFile = async (filePath: string): Promise<Buffer> => {
-  return await fs.readFile(filePath);
+export const loadFile = async (fileName: string, directory: string): Promise<Buffer> => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+    return await fsPromise.readFile(`./${directory}/${fileName}`);
+  } else {
+    return await fsPromise.readFile(`./${directory}/${fileName}`);
+  }
+}
+
+export const writeFile = async (fileName: string, directory: string, buffer: Buffer): Promise<void> => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+    return await fsPromise.writeFile(`./${directory}/${fileName}`, buffer);
+  } else {
+    return await fsPromise.writeFile(`./${directory}/${fileName}`, buffer);
+  }
 }
