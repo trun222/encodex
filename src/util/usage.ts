2a9b5212cb79f047bb222e3e8e5cd5fa3b7643b2
trun222 <thomasunderwoodii@gmail.com>
2022-11-17 13:53:43 -0500

    Updated Usages back to 10MB

--- src/util/usage.ts
+++ src/util/usage.ts
@@ -2,7 +2,7 @@ import { UsageType } from '@/src/db/User.prisma';
 
 export const UsageLimits = {
   // TODO: Adjust back for launch
-  free: { api: 10_000, storage: 0, maxFileSize: 20 * 1024 * 1024 },
+  free: { api: 10_000, storage: 0, maxFileSize: 10 * 1024 * 1024 },
   premium: { api: 50_000, storage: 50, maxFileSize: 20 * 1024 * 1024 },
   pro: { api: 100_000, storage: 500, maxFileSize: 100 * 1024 * 1024 },
 }
